36093793833fa17f888ec09b050173b69d9df7df
HampVR <hampton.moseley@gmail.com>
2023-01-13 13:47:45 -0600

    added qvpens

---
+++ HampVRWorldUnityProject/Assets/_Hamp/Scripts/lightMoving.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class lightMoving : UdonSharpBehaviour
+{
+  public float distanceToCover;
+  public float speed;
+
+    private Vector3 startingPosition;
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startingPosition;
+        v.x += distanceToCover * Mathf.Sin(Time.time * speed);
+        v.y += distanceToCover * Mathf.Cos(Time.time * speed);
+        v.z += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+
+    
+}
