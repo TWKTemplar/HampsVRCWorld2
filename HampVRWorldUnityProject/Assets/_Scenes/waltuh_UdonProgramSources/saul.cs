87f53d528e64155e76a6f03929a7964586d05429
HampVR <hampton.moseley@gmail.com>
2023-01-14 15:55:17 -0600

    added bottles
    added bottles and moved mike and saul

---
+++ HampVRWorldUnityProject/Assets/_Scenes/waltuh_UdonProgramSources/saul.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class saul : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+   public void Interact()
+        {
+
+        }
+}
